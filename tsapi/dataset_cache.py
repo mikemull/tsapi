eac85ab0dedcbfa73a8b094423d79167f89e6aa6
Mike Mull <mike.mull@gmail.com>
2025-03-28 10:12:08 -0700

    Add some error handling in cache calls

--- tsapi/dataset_cache.py
+++ tsapi/dataset_cache.py
@@ -6,26 +6,33 @@ import polars as pl
 
 class DatasetCache:
 
-    def __init__(self, settings):
+    def __init__(self, settings, logger):
         self.client = redis.Redis(host=settings.redis_host, port=6379, db=0)
+        self.logger = logger
 
     async def get_cached_dataset(self, dataset_id) -> pl.DataFrame:
         """
         Retrieve a cached dataset by its ID.
         """
-        cached_data = await self.client.get(dataset_id)
-        if cached_data is None:
-            return None
+        try:
+            cached_data = await self.client.get(dataset_id)
+            if cached_data is None:
+                return None
 
-        datasetio = io.BytesIO(cached_data)
-        dataframe = pl.read_ipc(datasetio)
-        return dataframe
+            datasetio = io.BytesIO(cached_data)
+            dataframe = pl.read_ipc(datasetio)
+            return dataframe
+        except Exception as e:
+            self.logger.error(f"Error retrieving cached dataset: {e}")
+            return None
 
     async def cache_dataset(self, dataset_id: str, dataframe: pl.DataFrame):
         """
         Cache a dataset by its ID.
         """
-
-        datasetio = io.BytesIO()
-        dataframe.write_ipc(datasetio, compression='lz4')
-        await self.client.set(dataset_id, datasetio.getvalue(), ex=3600)
+        try:
+            datasetio = io.BytesIO()
+            dataframe.write_ipc(datasetio, compression='lz4')
+            await self.client.set(dataset_id, datasetio.getvalue(), ex=3600)
+        except Exception as e:
+            self.logger.error(f"Error caching dataset: {e}")
